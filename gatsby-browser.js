8b916b0ee927b195ea32639521520d0b835f39c6
Rezso Dubiczki <rezso.dubiczki@dreamworld.com.au>
2023-03-28 10:39:47 +1000

    preview fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,3 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-
-// exports.onRenderBody = ({ setHtmlAttributes }) => {
-//   setHtmlAttributes({ lang: `en` })
-// }
 import * as React from 'react'
 import {
   PrismicPreviewProvider,
