fe6965464ad1043a6cf96111afe83fdf5dcb2057
Rezso Dubiczki <rezso.dubiczki@dreamworld.com.au>
2023-04-03 10:36:12 +1000

    preview test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,24 +1,24 @@
-import * as React from 'react'
-import {
-  PrismicPreviewProvider,
-  componentResolverFromMap,
-} from 'gatsby-plugin-prismic-previews'
+// import * as React from 'react'
+// import {
+//   PrismicPreviewProvider,
+//   componentResolverFromMap,
+// } from 'gatsby-plugin-prismic-previews'
 
-import { linkResolver } from './src/linkResolver'
-import PageTemplate from './src/pages/index'
+// import { linkResolver } from './src/linkResolver'
+// import PageTemplate from './src/pages/index'
 
-export const wrapRootElement = ({ element }) => (
-  <PrismicPreviewProvider
-    repositoryConfigs={[
-      {
-        repositoryName: "skypoint-dev",
-        linkResolver,
-        componentResolver: componentResolverFromMap({
-          page: PageTemplate,
-        }),
-      },
-    ]}
-  >
-    {element}
-  </PrismicPreviewProvider>
-)
+// export const wrapRootElement = ({ element }) => (
+//   <PrismicPreviewProvider
+//     repositoryConfigs={[
+//       {
+//         repositoryName: "skypoint-dev",
+//         linkResolver,
+//         componentResolver: componentResolverFromMap({
+//           page: PageTemplate,
+//         }),
+//       },
+//     ]}
+//   >
+//     {element}
+//   </PrismicPreviewProvider>
+// )
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,27 +7,29 @@
 // exports.onRenderBody = ({ setHtmlAttributes }) => {
 //   setHtmlAttributes({ lang: `en` })
 // }
-import * as React from 'react'
-import {
-  PrismicPreviewProvider,
-  componentResolverFromMap,
-} from 'gatsby-plugin-prismic-previews'
 
-import { linkResolver } from './src/linkResolver'
-import PageTemplate from './src/pages/index'
 
-export const wrapRootElement = ({ element }) => (
-  <PrismicPreviewProvider
-    repositoryConfigs={[
-      {
-        repositoryName: "skypoint-dev",
-        linkResolver,
-        componentResolver: componentResolverFromMap({
-          page: PageTemplate,
-        }),
-      },
-    ]}
-  >
-    {element}
-  </PrismicPreviewProvider>
-)
+// import * as React from 'react'
+// import {
+//   PrismicPreviewProvider,
+//   componentResolverFromMap,
+// } from 'gatsby-plugin-prismic-previews'
+
+// import { linkResolver } from './src/linkResolver'
+// import PageTemplate from './src/pages/index'
+
+// export const wrapRootElement = ({ element }) => (
+//   <PrismicPreviewProvider
+//     repositoryConfigs={[
+//       {
+//         repositoryName: "skypoint-dev",
+//         linkResolver,
+//         componentResolver: componentResolverFromMap({
+//           page: PageTemplate,
+//         }),
+//       },
+//     ]}
+//   >
+//     {element}
+//   </PrismicPreviewProvider>
+// )
