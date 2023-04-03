f06fd906ee8cdfec72b04495e6d008ecd20f9c61
Rezso Dubiczki <rezso.dubiczki@dreamworld.com.au>
2023-04-03 15:04:26 +1000

    fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,7 @@ exports.createPages = async ({ graphql, actions }) => {
     query {
       allPrismicSubpage {
         nodes {
+          id
           uid
           url
         }
@@ -16,22 +17,11 @@ exports.createPages = async ({ graphql, actions }) => {
   console.log("QR",queryResult.data.allPrismicSubpage.nodes)
 
   const productTemplate = path.resolve(`src/templates/using-dsg.js`)
-  // queryResult.data.allPrismicSubpage.nodes.forEach(node => {
-  //   createPage({
-  //     path: uid,
-  //     component: productTemplate,
-  //     context: {
-  //       // This time the entire product is passed down as context
-  //       product: node,
-  //     },
-  //   })
-  // })
-
   for (const page of queryResult.data.allPrismicSubpage.nodes ?? []) {
       createPage({
-        path: page.uid,
-        component: path.resolve(__dirname, "./src/templates/using-dsg.js"),
-        context: { id: page.uid },
+        path: page.url,
+        component: productTemplate,
+        context: { id: page.id },
       });
     }
 }
