3537ce83857114fba284b1a758c4716dafe29ef2
Rezso Dubiczki <rezso.dubiczki@dreamworld.com.au>
2023-04-06 08:46:45 +1000

    repositoryConfigs setup

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,31 +1,24 @@
 import * as React from 'react'
-import { PrismicPreviewProvider } from 'gatsby-plugin-prismic-previews'
+import {
+  PrismicPreviewProvider,
+  componentResolverFromMap,
+} from 'gatsby-plugin-prismic-previews'
+
+import { linkResolver } from './src/linkResolver'
+import PageTemplate from './src/components/layout'
 
 export const wrapRootElement = ({ element }) => (
-  <PrismicPreviewProvider>{element}</PrismicPreviewProvider>
+  <PrismicPreviewProvider
+    repositoryConfigs={[
+      {
+        repositoryName: "skypoint-dev",
+        linkResolver,
+        componentResolver: componentResolverFromMap({
+          page: PageTemplate,
+        }),
+      },
+    ]}
+  >
+    {element}
+  </PrismicPreviewProvider>
 )
-
-// import * as React from 'react'
-// import {
-//   PrismicPreviewProvider,
-//   componentResolverFromMap,
-// } from 'gatsby-plugin-prismic-previews'
-
-// import { linkResolver } from './src/linkResolver'
-// import PageTemplate from './src/components/layout'
-
-// export const wrapRootElement = ({ element }) => (
-//   <PrismicPreviewProvider
-//     repositoryConfigs={[
-//       {
-//         repositoryName: "skypoint-dev",
-//         linkResolver,
-//         componentResolver: componentResolverFromMap({
-//           page: PageTemplate,
-//         }),
-//       },
-//     ]}
-//   >
-//     {element}
-//   </PrismicPreviewProvider>
-// )
