0896404733b2ab1efe53e8319cf9d729a89215da
Pawichi <pawichi@192.168.1.5>
2023-04-19 14:26:54 -0600

    skypoint-devp in both

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,7 +11,7 @@ export const wrapRootElement = ({ element }) => (
   <PrismicPreviewProvider
     repositoryConfigs={[
       {
-        repositoryName: "skypoint-dev",
+        repositoryName: "skypoint-devp",
         linkResolver,
         componentResolver: componentResolverFromMap({
           subpage: PageTemplate,
